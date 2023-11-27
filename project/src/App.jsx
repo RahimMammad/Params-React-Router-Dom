4d9fa8b940ac3ba0893339dc6e2ad381939c62c1
emirxanmmdv <tu6xqhcdu@code.edu.az>
2023-11-27 12:20:54 +0400

    Param2

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,14 +1,14 @@
 // App.js
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import Layout from './Layout';
-import Home from './Home';
-import Contact from './Contact';
-import About from './About';
-import Products from './Products';
-import ProductDetail from './ProductDetail';
+import Navbar from './layouts/Navbar';
+import Footer from './layouts/Footer';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Contact from './pages/Contact/Contact';
+import About from './pages/About';
+import Products from './pages/Product/Products';
+import ProductDetail from './pages/ProductDetail';
 
 const App = () => {
   return (
